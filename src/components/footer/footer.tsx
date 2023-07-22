e10e01279781fe14bbbee798ad50568b74e7e63f
Sajjad Soudani <sajjad.soudani.ahwaz@gmail.com>
2023-07-22 13:33:54 +0330

    simple footer design

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,8 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <footer>
+    <footer class="w-full absolute top-100% p-4 text-center text-dark bg-cyan">
+      <div>Built by ME</div>
     </footer>
   );
 });
