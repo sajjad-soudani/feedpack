cad612030c7f4882ae6a7143fad23c4df14f647f
Sajjad Soudani <sajjad.soudani.ahwaz@gmail.com>
2023-07-22 16:58:39 +0330

    header styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,20 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <header></header>;
+  return (
+    <header class="p-4 flex flex-row justify-between bg-cyan">
+      <div class="flex-none">LOGO</div>
+        <ul class="w-1/2 flex justify-around list-none">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+        </ul>
+    </header>
+  );
 });
